c4d22b527443fd9ff9775a0ee0ea825121a091a8
chrmod <chrmod@chrmod.net>
2016-07-17 23:41:58 +0200

    [WIP] Transfer configuration

--- src/tests/transfer-test.js
+++ src/tests/transfer-test.js
@@ -29,10 +29,92 @@ describe("RequestTransfer", function () {
     });
   });
 
+  describe(".fromString", function () {
+    it("constructs transfer object", function () {
+      expect(RequestTransfer.fromString('{"fnName": "action"}')).to.be.instanceOf(RequestTransfer);
+    });
+
+    it("sets all core properties", function () {
+      const msg = {
+        id: 1,
+        fnName: "set",
+        fnArgs: [1,2,3],
+        wrapperId: 2,
+      };
+      const transfer = RequestTransfer.fromString(JSON.stringify(msg));
+      expect(transfer).to.have.property("id").deep.equal(msg.id);
+      expect(transfer).to.have.property("fnName").deep.equal(msg.fnName);
+      expect(transfer).to.have.property("fnArgs").deep.equal(msg.fnArgs);
+      expect(transfer).to.have.property("wrapperId").deep.equal(msg.wrapperId);
+    });
+
+    it("sets aliased properties", function () {
+      const msg = {
+        id: 1,
+        action: "set",
+        args: [1,2,3],
+        wrapId: 2,
+      };
+      const config = {
+        requestProperties: {
+          fnName: "action",
+          fnArgs: "args",
+          wrapperId: "wrapId",
+        }
+      };
+      const transfer = RequestTransfer.fromString(JSON.stringify(msg), config);
+      expect(transfer).to.property("id").equal(msg.id);
+      expect(transfer).to.property("fnName").deep.equal(msg[config.requestProperties.fnName]);
+      expect(transfer).to.property("fnArgs").deep.equal(msg[config.requestProperties.fnArgs]);
+      expect(transfer).to.property("wrapperId").deep.equal(msg.wrapId);
+    });
+  });
+
   describe("#toString", function () {
-    it("aliases JSON.stringify", function () {
-      let transfer = new RequestTransfer("test", [1,2,3]);
-      expect(JSON.parse(transfer.toString())).to.deep.equal(transfer);
+    it("stringify core properties", function () {
+      const transfer = new RequestTransfer("test", [1,2,3]);
+      expect(JSON.parse(transfer.toString())).to.deep.equal({
+        id: transfer.id,
+        fnName: transfer.fnName,
+        fnArgs: transfer.fnArgs,
+      });
+    });
+
+    it("aliases core properties based on config", function () {
+      const requestProperties = {
+        fnName: "action",
+        fnArgs: "args",
+      };
+      const transfer = new RequestTransfer("test", [1,2,3], { requestProperties });
+      transfer.wrapperId = 123;
+      expect(JSON.parse(transfer.toString())).to.deep.equal({
+        id: transfer.id,
+        [requestProperties.fnName]: transfer.fnName,
+        [requestProperties.fnArgs]: transfer.fnArgs,
+        wrapperId: transfer.wrapperId,
+      });
+    });
+
+    it("ignores additional properties", function () {
+      const transfer = new RequestTransfer("test", [1,2,3]);
+      transfer.someProperty = "someValue";
+      expect(JSON.parse(transfer.toString())).to.deep.equal({
+        id: transfer.id,
+        fnName: transfer.fnName,
+        fnArgs: transfer.fnArgs,
+      });
+    });
+
+    it("passes meta from configuration object", function () {
+      const meta = { target: "spanan", module: "core" };
+      const transfer = new RequestTransfer("test", [1,2,3], { meta });
+      expect(JSON.parse(transfer.toString())).to.deep.equal({
+        id: transfer.id,
+        fnName: transfer.fnName,
+        fnArgs: transfer.fnArgs,
+        target: meta.target,
+        module: meta.module,
+      });
     });
   });
 
--- src/transfer.js
+++ src/transfer.js
@@ -10,12 +10,57 @@ class BaseTransfer {
   }
 }
 
+const DEFAULT_CONFIG = {
+  requestProperties: {
+    id: "id",
+    fnName: "fnName",
+    fnArgs: "fnArgs",
+    wrapperId: "wrapperId",
+    transferId: "transferId",
+  },
+  meta: {}
+};
+
 export class RequestTransfer extends BaseTransfer {
 
-  constructor(fnName, fnArgs = []) {
+  constructor(fnName, fnArgs = [], config = {}) {
     super();
     this.fnName = fnName;
     this.fnArgs = Array.prototype.slice.call(fnArgs);
+    this.config = {
+      requestProperties: Object.assign({}, DEFAULT_CONFIG.requestProperties, config.requestProperties),
+      meta: Object.assign({}, DEFAULT_CONFIG.meta, config.meta),
+    };
+  }
+
+  toString() {
+    const transfer = Object.assign({
+      [this.config.requestProperties.id]:     this.id,
+      [this.config.requestProperties.fnName]: this.fnName,
+      [this.config.requestProperties.fnArgs]: this.fnArgs,
+      [this.config.requestProperties.wrapperId]: this.wrapperId,
+      [this.config.requestProperties.transferId]: this.transferId,
+    }, this.config.meta);
+    return JSON.stringify(transfer);
+  }
+
+  static fromString(str, config = {}) {
+    const msg = JSON.parse(str);
+    const requestProperties = Object.assign(
+      {},
+      DEFAULT_CONFIG.requestProperties,
+      config.requestProperties || {}
+    );
+    const transfer = new RequestTransfer(
+      msg[requestProperties.fnName],
+      msg[requestProperties.fnArgs],
+      config
+    );
+    Object.keys(requestProperties).forEach(propName => {
+      const alias = requestProperties[propName];
+      transfer[propName] = msg[alias];
+    });
+    return transfer;
   }
 }
 
